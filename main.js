596e659dcdeea9957223666c9d6e6f8ee86c3f30
mahiizia <165680433+mahiizia@users.noreply.github.com>
2024-04-02 09:08:05 -0700

    Add files via upload
    explain data types

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+// Core Data Types
+// 1) string
+/* strings are values that are written inside "" or '', representating textual data.
+   Anything written inside "" or '' will be considered as string e.g:("abc", "123", "true")
+*/
+var stringTypeExample = "This is a string";
+console.log(stringTypeExample);
+// 2) number
+/* Number type is fundamental for representing numeric data and performing mathematical
+   operations within JavaScript programs. e.g:(1,10,100,100)
+*/
+var numberTypeExample = 100;
+console.log(numberTypeExample);
+// 3) boolean true and false
+/* the boolean type represents a logical value that can be either true or false. Booleans
+   are commonly used for conditions, comparisons, and logical operations.
+*/
+var booleanTypeExample1 = true;
+console.log(booleanTypeExample1);
+var booleanTypeExample2 = false;
+console.log(booleanTypeExample1);
---
+++ main.ts
@@ -0,0 +1,23 @@
+
+"use strict";
+// Core Data Types
+// 1) string
+/* strings are values that are written inside "" or '', representating textual data.
+   Anything written inside "" or '' will be considered as string e.g:("abc", "123", "true")
+*/
+let stringTypeExample = "This is a string";
+console.log(stringTypeExample);
+// 2) number
+/* Number type is fundamental for representing numeric data and performing mathematical
+   operations within JavaScript programs. e.g:(1,10,100,100)
+*/
+let numberTypeExample = 100;
+console.log(numberTypeExample);
+// 3) boolean true and false
+/* the boolean type represents a logical value that can be either true or false. Booleans
+   are commonly used for conditions, comparisons, and logical operations.
+*/
+let booleanTypeExample1 = true;
+console.log(booleanTypeExample1);
+let booleanTypeExample2 = false;
+console.log(booleanTypeExample1);
