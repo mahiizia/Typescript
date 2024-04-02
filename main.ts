53919cdaa9e1399b5072348f1fe389febb3a1fec
mahiizia <165680433+mahiizia@users.noreply.github.com>
2024-04-02 08:53:50 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,49 @@
+ //  ARTHEMETIC OPERATORS 
+  //  URANY OP
+  //  ASSIGMENT OP
+  //  COMPARISON OP
+
+
+let numberOfFaculty = 4;
+let numberOfManagment = 3;
+let numberOfAbsentFaculty = 2;
+let numberOfBatches = 21
+let numberOfTotalFaculty = numberOfFaculty * 21
+let totalNumberOfKgs = 20;
+
+console.log(totalNumberOfKgs/numberOfTotalFaculty);
+
+// let num1 = 5;
+// --num1;
+// --num1;
+// --num1;
+// --num1;
+// --num1;
+// --num1;
+// console.log(num1);l
+
+//  let i = 10;
+// //            11 + 12 + 6 + 11 -2
+// let result = ++i + ++i + 6 + --i - 2;
+//  console.log(result);
+//  BMI : bodey mass index 
+
+//  let firstName = "maheen";
+//  let age = 20;
+
+//  console.log (`my name is: ${firstName} and my age is: ${age}`)
+//  let a:string = "maheen";
+//  Num 1 = 4;
+//  console.log (num1);
+
+  // let numberOfFaculty = 7;
+  // let numberOfBatches = 9;
+  // let numberOfAbsentFaculty = 5
+
+  // console.log (numberOfFaculty+numberOfBatches-numberOfAbsentFaculty);
+  
+  // let a: number=5;
+  // let b:number=2;
+  // let c: number;
+  // c= ++a + a++ + --b + b-- + a + b++ + b;
+  // console.log(c);
